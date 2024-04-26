d4e3c2af22539a43e3ab5cf632f7d1656cb4acfd
mudar-hussain <mudar.shussain@gmail.com>
2024-04-26 18:22:29 +0530

    Header enhancement

--- src/app/common/layouts/header/header.component.ts
+++ src/app/common/layouts/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component, ViewChild  } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isWindow: boolean = window.innerWidth < 770;
+  
+  ngOnInit(): void {
+    console.log(window.innerWidth);
+  }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isWindow = event.target.innerWidth < 770 ? true : false;
+  }
 }
